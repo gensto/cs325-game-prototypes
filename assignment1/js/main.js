6e619fd5ef76f60e9d8f2503e7348a5cffff4c11
gensto <47303604+gensto@users.noreply.github.com>
2019-02-07 11:55:27 -0500

    update

--- assignment1/js/main.js
+++ assignment1/js/main.js
@@ -27,8 +27,8 @@ window.onload = function() {
     var rockSprite;
     var numberOfLives;
     var numberOfLivesText
-    var score;
-    var scoreCountText;
+    //var score;
+    //var scoreCountText;
     var gameOverText;
     var moveDownThisX; //have the rock move down this X lane
 
@@ -50,7 +50,7 @@ window.onload = function() {
         //hiding the game over text by displaying it off the screen, only solution i could think of
 
 
-        //background = game.add.image(60, 0, 'background');
+        background = game.add.image(60, 0, 'background');
         background.scale.x = 0.8;
 
         numberOfLives = 3;
@@ -58,8 +58,6 @@ window.onload = function() {
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Dodge that rock!.", style );
         numberOfLivesText = game.add.text( game.world.centerX + 300, 15, numberOfLives, style );
-        scoreCountText = game.add.text( game.world.centerX + 300, 40, score, style );
-        scoreCountText.setText("Score: " + score);
         numberOfLivesText.setText("Lives: " + numberOfLives);
 
         text.anchor.setTo( 0.5, 0.0 );
@@ -127,12 +125,12 @@ window.onload = function() {
 
       numberOfLivesText.setText("Lives: " + numberOfLives);
     }
-
+/*
     function incrementScore()
     {
       score++;
       scoreCountText.setText("Score: " + score);
-    }
+    }*/
 
     function spawnRock()
     {
@@ -149,7 +147,7 @@ window.onload = function() {
       if(rockSprite.y > 600)
       {
         rockSprite.kill();
-        incrementScore(); //increment score as soon as rock passes player
+        //incrementScore(); //increment score as soon as rock passes player
         spawnRock();
       }
 
