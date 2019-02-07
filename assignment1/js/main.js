31db84508f57ec17e33677d428052b0df11b149a
gensto <47303604+gensto@users.noreply.github.com>
2019-02-07 11:38:04 -0500

    update

--- assignment1/js/main.js
+++ assignment1/js/main.js
@@ -27,7 +27,7 @@ window.onload = function() {
     var rockSprite;
     var numberOfLives;
     var numberOfLivesText
-    var score;
+    var scoreCount;
     var scoreCountText;
     var gameOverText;
     var moveDownThisX; //have the rock move down this X lane
@@ -58,9 +58,9 @@ window.onload = function() {
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Dodge that rock!.", style );
         numberOfLivesText = game.add.text( game.world.centerX + 300, 15, numberOfLives, style );
-        scoreCountText = game.add.text( game.world.centerX + 290, 40, score, style );
-        scoreCountText.setText("Score: " + score);
+        scoreCountText = game.add.text( game.world.centerX + 290, 40, scoreCount, style );
         numberOfLivesText.setText("Lives: " + numberOfLives);
+        scoreCountText.setText("Score: " + scoreCount);
 
         text.anchor.setTo( 0.5, 0.0 );
 
@@ -130,8 +130,8 @@ window.onload = function() {
 
     function incrementScore()
     {
-      score++;
-      scoreCountText.setText("Score: " + score);
+      scoreCount++;
+      scoreCountText.setText("Score: " + scoreCount);
     }
 
     function spawnRock()
